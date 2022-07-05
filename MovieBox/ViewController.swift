63389c8f9f632b0ef1f0a93b5b9dd50ad239f67f
miggelucas <miggelucas@Mig-MacBook-Pro.local>
2022-07-05 17:50:44 -0300

    Adicionando primeira tela no storyBoard

---
+++ MovieBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBox
+//
+//  Created by Lucas Migge de Barros on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
